b9cae4e09cc57d02fcddcec6377a64817cebaf4b
kmd2zjw <98543353+kmd2zjw@users.noreply.github.com>
2023-04-04 19:54:46 -0400

    added router for page urls.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    childern: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/profile",
+        element: <Profile/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router = {router}/>
     </div>
   );
 }
+
 
 export default App;
