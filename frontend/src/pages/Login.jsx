9a991af40518ec996390ecc41a5c0fe613558232
kmd2zjw <98543353+kmd2zjw@users.noreply.github.com>
2023-04-06 17:01:42 -0400

    added login and logout
    from tutorial

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,55 @@
-import React from "react";
-import {Link} from "react-router-dom";
-//import './Login.scss';
+import axios from "axios";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+
 const Login = () => {
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+      });
+      const [err, setError] = useState(null);
+    
+      const navigate = useNavigate();
+    
+      const { login } = useContext(AuthContext);
+    
+    
+      const handleChange = (e) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          await login(inputs)
+          navigate("/");
+        } catch (err) {
+          setError(err.response.data);
+        }
+      };
+
     return (
         <div className="auth">
             <h1>Login</h1>
             <form>
-                <input required type="email" placeholder='Email' />
-                <input required type="password" placeholder='Password' />
-                <button>Login</button>
+                <input  
+                    required
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                    onChange={handleChange}
+                />
+                <input 
+                    required
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    onChange={handleChange}
+                />
+                <button onClick={handleSubmit}>Login</button>
+                {err && <p>{err}</p>}
                 <p>Failed to login!</p>
                 <span>Don't have an account yet? <Link to ="/register">Register</Link></span>
             </form>
@@ -16,4 +57,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Login;
