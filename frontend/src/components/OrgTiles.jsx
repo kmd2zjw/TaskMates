e5041cb325e7f03962b9ae83e2c63a1f9ff76e7e
ajk3158 <71356356+ajk3158@users.noreply.github.com>
2023-04-26 13:47:51 -0400

    Styled pages (unfinished

--- frontend/src/components/OrgTiles.jsx
+++ frontend/src/components/OrgTiles.jsx
@@ -1,36 +1,40 @@
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
+import Typography from '@mui/material/Typography';
+import Button from "@mui/material/Button";
 
 const OrgTiles = () => {
-    const [orgs, setOrgs] = useState([]);
+  const [orgs, setOrgs] = useState([]);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const res = await axios.get(`/orgs/`);
-          setOrgs(res.data);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      fetchData();
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/orgs/`);
+        setOrgs(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  });
 
-    return (
-        <div className="menu">
-          <h1>Orgs you're apart of: </h1>
-          {orgs.map((org) => (
-            <div className="org" key={org.groupID}>
-              <h2>{org.groupName}</h2>
-              <Link className="link" to={`/orgs/${org.groupID}`}>
-                <h2>Click here</h2>
-              </Link>
-            </div>
-          ))}
+  return (
+    <div className="app__org-menu">
+      <Typography variant="h3">Orgs you're apart of: </Typography>
+      {orgs.map((org) => (
+        <div className="app__org" key={org.groupID}>
+          <Typography variant="h4">{org.groupName}</Typography>
+          <Link className="link" to={`/orgs/${org.groupID}`}>
+            <Button variant="outlined" sx={{ color: '#212121', borderColor: '#212121' }} className="app__section-element">
+              <Typography variant="h4">Click here</Typography>
+            </Button>
+          </Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default OrgTiles
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Button from "@mui/material/Button";
+import Typography from '@mui/material/Typography';
+import { AppWrap } from '../wrapper';
+
+import {useContext} from "react";
+import {AuthContext} from "../context/authContext"
 
 const Profile = () => {
+
+    const { currentUser } = useContext(AuthContext);
+
+
     return (
-        <div>Profile</div>
+        <Box className="app__container" style={{justifyContent: "flex-start"}}>
+            {currentUser ? (
+                <Typography variant="h2" className="title-text app__profile-element">{currentUser?.first_name} {currentUser?.last_name}</Typography>
+            ) : (
+                <Typography variant="h2">Please login in order to view profile</Typography>
+            )}
+
+                {currentUser ? (
+                    <div>
+                    <Typography variant="h4" className="app__profile-element">Currently Involved orgs</Typography>
+                    
+
+                    <Typography variant="h4" className="app__profile-element">Email: {currentUser?.email}</Typography>
+
+                    <Typography variant="h4" className="app__profile-element">Phone Number: {currentUser?.phone_number}</Typography>
+                    </div>
+                ) : (
+                    <Typography variant="h5">*Login using the Login button in the Navbar</Typography>
+                )}
+
+        </Box>
     )
 }
 
-export default Profile
+export default AppWrap(Profile, "Profile");
