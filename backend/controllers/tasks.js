149bb2232689da74de1519e20994c82e942186e4
ajk3158 <71356356+ajk3158@users.noreply.github.com>
2023-04-28 20:24:52 -0400

    Revised organization page

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,55 +1,55 @@
-import {db} from "../db.js"
+import { db } from "../db.js"
 import jwt from "jsonwebtoken";
 
-export const addTask = (req, res) =>{
-    const token = req.cookies.access_token;
-    if (!token) return res.status(401).json("Not authenticated!");
-    jwt.verify(token, "jwtkey", (err, userInfo) => {
-      if (err) return res.status(403).json("Token is not valid!");
-      const values = [
-        userInfo.id,
-        req.body.date,
-        req.body.description,
-        req.body.name,
-      ]
-      const q1 = "INSERT INTO task(`assigner_userID`, `due_date`, `description`, `task_name`) VALUES (?)";
-      db.query(q1, [values], (err,data)=>{
-          if(err) return res.send(err);
-          const q2 = "SELECT taskID from task WHERE task_name = ?";
-          db.query(q2, [req.body.name], (err1,data1)=>{
-            if(err1) return res.send(err1);
-            const temp = [
-                data1[0].taskID,
-                req.body.orgId,
-            ]
-            const q3 = "INSERT INTO group_tasks(`taskID`, `groupID`) VALUES (?)"
-            db.query(q3, [temp], (err2, data2)=> {
-                if(err2) return res.send(err2);
-            })
-          });
-
-          return res.json("Task has been created");
-          
+export const addTask = (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+    const values = [
+      userInfo.id,
+      req.body.date,
+      req.body.description,
+      req.body.name,
+    ]
+    const q1 = "INSERT INTO task(`assigner_userID`, `due_date`, `description`, `task_name`) VALUES (?)";
+    db.query(q1, [values], (err, data) => {
+      if (err) return res.send(err);
+      const q2 = "SELECT taskID from task WHERE task_name = ?";
+      db.query(q2, [req.body.name], (err1, data1) => {
+        if (err1) return res.send(err1);
+        const temp = [
+          data1[0].taskID,
+          req.body.orgId,
+        ]
+        const q3 = "INSERT INTO group_tasks(`taskID`, `groupID`) VALUES (?)"
+        db.query(q3, [temp], (err2, data2) => {
+          if (err2) return res.send(err2);
+        })
       });
+
+      return res.json("Task has been created");
+
     });
+  });
 }
 
-export const getUserTasks = (req, res) =>{
-    const token = req.cookies.access_token;
-    if (!token) return res.status(401).json("Not authenticated!");
-    jwt.verify(token, "jwtkey", (err, userInfo) => {
-      if (err) return res.status(403).json("Token is not valid!");
-      const q = "SELECT * FROM task INNER JOIN assigned_to ON assigned_to.taskID = task.taskID where userID=?"
-      db.query(q, [userInfo.id], (err, data) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json(data);
-      })
-      
-    });
+export const getUserTasks = (req, res) => {
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+    const q = "SELECT * FROM (task NATURAL JOIN group_tasks NATURAL JOIN organization) INNER JOIN assigned_to ON assigned_to.taskID = task.taskID where userID=?"
+    db.query(q, [userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    })
+
+  });
 }
 
-export const getGroupTasks = (req, res) =>{
-    const q =
+export const getGroupTasks = (req, res) => {
+  const q =
     "SELECT * FROM group_tasks NATURAL JOIN task WHERE groupID = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
@@ -58,7 +58,7 @@ export const getGroupTasks = (req, res) =>{
   });
 }
 
-export const acceptTask = (req, res) =>{
+export const acceptTask = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json("Not authenticated!");
   jwt.verify(token, "jwtkey", (err, userInfo) => {
@@ -79,7 +79,7 @@ export const acceptTask = (req, res) =>{
   });
 }
 
-export const getTask = (req, res) =>{
+export const getTask = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json("Not authenticated!");
   jwt.verify(token, "jwtkey", (err, userInfo) => {
@@ -92,7 +92,7 @@ export const getTask = (req, res) =>{
   });
 }
 
-export const deleteTask = (req, res) =>{
+export const deleteTask = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json("Not authenticated!");
   jwt.verify(token, "jwtkey", (err1, userInfo) => {
@@ -107,7 +107,7 @@ export const deleteTask = (req, res) =>{
           if (err) return res.status(500).json(err);
           return res.status(200).json("Task deleted");;
         })
-        
+
       })
     })
 
